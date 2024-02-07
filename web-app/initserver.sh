ad298a63a5ba2c42c65f30ebe500f485ebd1da36
kl461 <kaixin.lu@duke.edu>
2024-02-07 18:05:06 -0500

    permission

--- web-app/initserver.sh
+++ web-app/initserver.sh
@@ -2,7 +2,8 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 chmod 777 ./token.json
-chmod 777 ./web-app/*.sres="$?"
+chmod 777 ./danger_log.txt
+res="$?"
 while [ "$res" != "0" ]
 do
     sleep 3;
