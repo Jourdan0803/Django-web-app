4317b95ab2638b311821e48cd2caea25287bea52
jourdan <lijingxuan980803@163.com>
2024-02-21 15:03:34 -0500

    delete token

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# Django-web-app
+# Django Web-App
+Duke ECE 568: Engineering Robust Server Software HW1. It can be used as a template for Django web project.
+
+⛳ This web-app assembles Uber, which lets users request, drive for, and join rides. It has three roles: Passenger, Driver, and Manager. The functionalities include:
+
+- **Create Account**
+- **Login/Logout**
+- **Driver Registration**
+- **Ride Selection**
+- **Ride Requesting**
+- **Ride Request Editing (Owner)**
+- **Ride Request Viewing (Owner / Sharer)**
+- **Ride Status Viewing (Driver)**
+- **Ride Searching (Driver)**
+- **Ride Searching (Sharer)**
+- **And some other unlist features...**
+
+
+![](img/1.png)
+
+**💠 Now enjoy this project!**
+
+## Demo
+### 1. Register
+![](assets/img/demo%20(2).png)
+![](assets/img/demo%20(3).png)
+
+### 2. Login
+![](assets/img/demo%20(1).png)
+
+### 3. Home Page
+![](assets/img/demo%20(4).png)
+
+### 4. Search
+![](assets/img/demo%20(5).png)
+![](assets/img/demo%20(6).png)
+
+### 5. Edit Profile
+![](assets/img/demo%20(7).png)
+
+### 6. Edit Vehicle Info
+![](assets/img/demo%20(8).png)
+
+### 7. View My Ride
+![](assets/img/demo%20(11).png)
+
+### 8. Search for Ride (Driver)
+![](assets/img/demo%20(12).png)
+![](assets/img/demo%20(13).png)
+
+### 9. View Ride
+![](assets/img/demo%20(14).png)
+![](assets/img/demo%20(15).png)
+
+### 10. Emails (OTP and Ride Confirmation)
+![](assets/img/demo%20(16).png)
+![](assets/img/demo%20(17).png)
+
+
+## Contribution
+**🔱 Developed by Jourdan.**
+
+Since I didn't really enroll in this course, this project is only used for self-learning. Some of this project requirements are meaningless and time-consuming for me, so I have not implemented (or just leave the interface) yet.
+
+**Due to the limited time, I did not clean up and refractor the code. I would be glad if someone can further improve this project.**
+
+📧 If you have any questions, feel free to contact me through:
+![](assets/img/demo%20(9).png)
--- web-app/polls/quickstart.py
+++ web-app/polls/quickstart.py
@@ -13,34 +13,9 @@ import google.auth
 # If modifying these scopes, delete the file token.json.
 # SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-
-# def gmail_authenticate():
-#   """Shows basic usage of the Gmail API.
-#   Lists the user's Gmail labels.
-#   """
-#   creds = None
-#   # The file token.json stores the user's access and refresh tokens, and is
-#   # created automatically when the authorization flow completes for the first
-#   # time.
-#   if os.path.exists("token.json"):
-#     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-#   # If there are no (valid) credentials available, let the user log in.
-#   if not creds or not creds.valid:
-#     if creds and creds.expired and creds.refresh_token:
-#       creds.refresh(Request())
-#     else:
-#       flow = InstalledAppFlow.from_client_secrets_file(
-#           "credentials.json", SCOPES
-#       )
-#       creds = flow.run_local_server(port=8000)
-#     # Save the credentials for the next run
-#     with open("token.json", "w") as token:
-#       token.write(creds.to_json())
-#   return build('gmail', 'v1', credentials=creds)
-
 # Authentication and service creation
 def gmail_authenticate():
-    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+    SCOPES = ["https://www.googleapis.com/auth/gmail.readonly","https://mail.google.com/"]
     creds = None
     # token.json stored user access and refresh tokens
     if os.path.exists('token.json'):
@@ -64,23 +39,6 @@ def gmail_authenticate():
 
     return build('gmail', 'v1', credentials=creds)
 
-  # try:
-  #   # Call the Gmail API
-  #   service = build("gmail", "v1", credentials=creds)
-  #   results = service.users().labels().list(userId="me").execute()
-  #   labels = results.get("labels", [])
-
-  #   if not labels:
-  #     print("No labels found.")
-  #     return
-  #   print("Labels:")
-  #   for label in labels:
-  #     print(label["name"])
-
-  # except HttpError as error:
-  #   # TODO(developer) - Handle errors from gmail API.
-  #   print(f"An error occurred: {error}")
-
 # Create and send emails
 def send_message(service, sender, to, subject, msg_html):
     message = MIMEMultipart('alternative')
@@ -99,49 +57,10 @@ def send_message(service, sender, to, subject, msg_html):
     print(f"Message Id: {message['id']}")
 
 # Using Gmail API
-# service = gmail_authenticate()
-# send_message(service, "jourdan.ljxx@gmail.com", "jingxuan.li@duke.edu", "Test Email", "<h1>Your ride has been confirmed by a driver</h1>")
+    # send_message(service, "jourdan.ljxx@gmail.com", "jingxuan.li@duke.edu", "Test Email", "<h1>Your ride has been confirmed by a driver</h1>")
 
-
-# def gmail_send_message():
-#   """Create and send an email message
-#   Print the returned  message id
-#   Returns: Message object, including message id
-
-#   Load pre-authorized user credentials from the environment.
-#   TODO(developer) - See https://developers.google.com/identity
-#   for guides on implementing OAuth2 for the application.
-#   """
-#   creds, _ = google.auth.default()
-
-#   try:
-#     service = gmail_authenticate()
-#     message = EmailMessage()
-
-#     message.set_content("This is automated draft mail")
-
-#     message["To"] = "jingxuan.li@duke.edu"
-#     message["From"] = "jourdan.ljxx@gmail.com"
-#     message["Subject"] = "Automated draft"
-
-#     # encoded message
-#     encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-#     create_message = {"raw": encoded_message}
-#     # pylint: disable=E1101
-#     send_message = (
-#         service.users()
-#         .messages()
-#         .send(userId="me", body=create_message)
-#         .execute()
-#     )
-#     print(f'Message Id: {send_message["id"]}')
-#   except HttpError as error:
-#     print(f"An error occurred: {error}")
-#     send_message = None
-#   return send_message
-
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
 #   gmail_authenticate()
-#   # gmail_send_message()
+  service = gmail_authenticate()
+  send_message(service, "jourdan.ljxx@gmail.com", "jingxuan.li@duke.edu", "Test Email", "<h1>Your ride has been confirmed by a driver</h1>")
+
